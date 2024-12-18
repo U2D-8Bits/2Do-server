1c8bb9b5677064c17fa9aba23fcc5a475ac405d3
U2D-8Bits <u2d.8bits@gmail.com>
2024-12-18 16:48:55 -0500

    add TypeORM integration to lists, subtasks, tasks, and users modules

--- src/lists/lists.module.ts
+++ src/lists/lists.module.ts
@@ -1,8 +1,14 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { ListsService } from './lists.service';
 import { ListsController } from './lists.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { List } from './entities/list.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([ List ]),
+  ],
   controllers: [ListsController],
   providers: [ListsService],
 })
--- src/subtask/subtask.module.ts
+++ src/subtask/subtask.module.ts
@@ -1,8 +1,17 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { SubtaskService } from './subtask.service';
 import { SubtaskController } from './subtask.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Subtask } from './entities/subtask.entity';
 
 @Module({
+  imports: [
+
+    TypeOrmModule.forFeature([
+      Subtask,
+    ]),
+  ],
   controllers: [SubtaskController],
   providers: [SubtaskService],
 })

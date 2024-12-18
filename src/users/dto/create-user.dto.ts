c8bfab99443f1344dccc16c90953227b23d78c64
U2D-8Bits <u2d.8bits@gmail.com>
2024-12-18 18:38:41 -0500

    add class-sanitizer for user DTO validation and enhance password requirements

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,15 +1,37 @@
 /* eslint-disable prettier/prettier */
-import { IsEmail, IsString, MinLength } from "class-validator";
+import { Trim } from 'class-sanitizer';
+import {
+  IsEmail,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class CreateUserDto {
+  @IsString()
+  @MinLength(3, {
+    message: 'El nombre de usuario debe tener al menos 3 caracteres',
+  })
+  @MaxLength(30, {
+    message: 'El nombre de usuario no puede tener más de 30 caracteres',
+  })
+  @Trim()
+  str_user_username: string;
 
-    @IsString()
-    str_user_username: string;
+  @IsEmail({}, { message: 'El correo electrónico debe ser válido' })
+  @Trim()
+  str_user_email: string;
 
-    @IsEmail()
-    str_user_email: string;
-
-    @IsString()
-    @MinLength(8)
-    str_user_password: string;
+  @IsString()
+  @MinLength(8, { message: 'La contraseña debe tener al menos 8 caracteres' })
+  @MaxLength(20, {
+    message: 'La contraseña no puede tener más de 20 caracteres',
+  })
+  @Matches(/(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*])/, {
+    message:
+      'La contraseña debe incluir al menos una letra mayúscula, una minúscula, un número y un carácter especial (!@#$%^&*)',
+  })
+  @Trim()
+  str_user_password: string;
 }

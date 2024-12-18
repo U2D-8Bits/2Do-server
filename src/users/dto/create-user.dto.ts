7fa745689426dce7b437f52ccd6df0ee61fe7cfd
U2D-8Bits <u2d.8bits@gmail.com>
2024-12-18 18:30:39 -0500

    add user DTOs and interfaces for user creation and login functionality

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,15 @@
-export class CreateUserDto {}
+/* eslint-disable prettier/prettier */
+import { IsEmail, IsString, MinLength } from "class-validator";
+
+export class CreateUserDto {
+
+    @IsString()
+    str_user_username: string;
+
+    @IsEmail()
+    str_user_email: string;
+
+    @IsString()
+    @MinLength(8)
+    str_user_password: string;
+}

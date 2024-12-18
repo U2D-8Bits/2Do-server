4bbf096bee0c53ee1926cccebe632c254233e96d
U2D-8Bits <u2d.8bits@gmail.com>
2024-12-18 15:54:50 -0500

    add environment configuration and enable CORS in the application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,34 @@
+/* eslint-disable prettier/prettier */
+
+
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppService } from './app.service';
 
+
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+
+    ConfigModule.forRoot({}),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.PGHOST,
+      port: parseInt(process.env.PGPORT),
+      username: process.env.PGUSER,
+      password: process.env.PGPASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+
+    TypeOrmModule.forFeature([]),
+
+  ],
+  controllers: [],
   providers: [AppService],
 })
 export class AppModule {}

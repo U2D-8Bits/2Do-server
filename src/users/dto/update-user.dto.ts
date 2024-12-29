a6a05f242d13a3414aa710d70aedd9dad252bdc6
U2D-8Bits <u2d.8bits@gmail.com>
2024-12-29 12:39:50 -0500

    add validation and sanitization to UpdateUserDto for email, username, and password fields

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,33 @@
+/* eslint-disable prettier/prettier */
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { IsEmail, IsString, Matches, MaxLength, MinLength } from 'class-validator';
+import { Trim } from 'class-sanitizer';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @IsEmail({}, { message: 'El correo electrónico debe ser válido' })
+  @Trim()
+  str_user_email: string;
+
+  @IsString()
+  @MinLength(3, {
+    message: 'El nombre de usuario debe tener al menos 3 caracteres',
+  })
+  @MaxLength(30, {
+    message: 'El nombre de usuario no puede tener más de 30 caracteres',
+  })
+  @Trim()
+  str_user_username: string;
+
+  @IsString()
+  @MinLength(8, { message: 'La contraseña debe tener al menos 8 caracteres' })
+  @MaxLength(20, {
+    message: 'La contraseña no puede tener más de 20 caracteres',
+  })
+  @Matches(/(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*])/, {
+    message:
+      'La contraseña debe incluir al menos una letra mayúscula, una minúscula, un número y un carácter especial (!@#$%^&*)',
+  })
+  @Trim()
+  str_user_password: string;
+}

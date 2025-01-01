a1b407ce428b6412e5694643a79217fcc30f0166
Pablo Israel Bolaños Párraga <u2d.8bits@gmail.com>
2025-01-01 13:55:03 -0500

    add FindTaskByUserDto for enhanced task retrieval filtering options

---
+++ src/task/dto/find-task-by-user.dto.ts
@@ -0,0 +1,42 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable prettier/prettier */
+
+//*------------------------------------------------------------------
+//* Import Dependency
+//*------------------------------------------------------------------
+import {
+  IsOptional,
+  IsBoolean,
+  IsNumber,
+  IsString,
+  Min,
+} from 'class-validator';
+
+//*------------------------------------------------------------------
+//* DTO Class
+//*------------------------------------------------------------------
+
+export class FindTaskByUserDto {
+  @IsOptional()
+  @IsNumber()
+  @Min(1)
+  page?: number;
+
+  @IsOptional()
+  @IsNumber()
+  @Min(1)
+  limit?: number;
+
+  @IsOptional()
+  @IsBoolean()
+  completed?: boolean;
+
+  @IsOptional()
+  @IsNumber()
+  priority?: number;
+
+  @IsOptional()
+  @IsString()
+  dueDate?: string;
+}

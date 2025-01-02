856a668cc824fd2509d5bced2353f113010924d5
Pablo Israel Bolaños Párraga <u2d.8bits@gmail.com>
2025-01-01 20:23:33 -0500

    add CreateSubtaskDto with validation for subtask title and task ID

--- src/subtask/dto/create-subtask.dto.ts
+++ src/subtask/dto/create-subtask.dto.ts
@@ -1,1 +1,23 @@
-export class CreateSubtaskDto {}
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+//*------------------------------------------------------------------
+//* Import Modules
+//*------------------------------------------------------------------
+
+import { IsNotEmpty, IsNumber, IsString } from "class-validator";
+
+//*------------------------------------------------------------------
+//* DTO Class
+//*------------------------------------------------------------------
+export class CreateSubtaskDto {
+
+    @IsString()
+    @IsNotEmpty()
+    str_subtask_title: string;
+
+    @IsNumber()
+    @IsNotEmpty()
+    int_task_id: number;
+
+}

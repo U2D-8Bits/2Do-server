f29100c6e0c6e6fb519bf9ccf839471de6167b6e
Pablo Israel Bolaños Párraga <u2d.8bits@gmail.com>
2025-01-06 10:09:08 -0500

    refactor: update ListsController to use number type for ID parameters and improve user-specific list retrieval

--- src/lists/lists.controller.ts
+++ src/lists/lists.controller.ts
@@ -1,34 +1,64 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+//*------------------------------------------------------------------
+//* Imports
+//*------------------------------------------------------------------
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { ListsService } from './lists.service';
 import { CreateListDto } from './dto/create-list.dto';
 import { UpdateListDto } from './dto/update-list.dto';
 
+//*------------------------------------------------------------------
+//* Controller Class
+//*------------------------------------------------------------------
 @Controller('lists')
 export class ListsController {
+
+  //*------------------------------------------------------------------
+  //* Constructor
+  //*----------------------------------------------------------------
   constructor(private readonly listsService: ListsService) {}
 
+  //*------------------------------------------------------------------
+  //* Create List
+  //*------------------------------------------------------------------
   @Post()
   create(@Body() createListDto: CreateListDto) {
     return this.listsService.create(createListDto);
   }
 
-  @Get()
-  findAll() {
-    return this.listsService.findAll();
+  //*------------------------------------------------------------------
+  //* Find All Lists by User ID
+  //*------------------------------------------------------------------
+  @Get('user/:id')
+  findAll(
+    @Param('id') id: number
+  ) {
+    return this.listsService.findAll(id);
   }
 
+  //*------------------------------------------------------------------
+  //* Find One List by ID
+  //*----------------------------------------------------------------
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.listsService.findOne(+id);
+  findOne(@Param('id') id: number) {
+    return this.listsService.findOne(id);
   }
 
+  //*------------------------------------------------------------------
+  //* Update List by ID
+  //*------------------------------------------------------------------
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateListDto: UpdateListDto) {
-    return this.listsService.update(+id, updateListDto);
+  update(@Param('id') id: number, @Body() updateListDto: UpdateListDto) {
+    return this.listsService.update(id, updateListDto);
   }
 
+  //*------------------------------------------------------------------
+  //* Remove List by ID
+  //*----------------------------------------------------------------
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.listsService.remove(+id);
+  remove(@Param('id') id: number) {
+    return this.listsService.remove(id);
   }
 }

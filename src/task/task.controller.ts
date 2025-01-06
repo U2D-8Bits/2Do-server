301ef680542850df838db753c5c92e5267adb245
Pablo Israel Bolaños Párraga <u2d.8bits@gmail.com>
2025-01-06 11:50:32 -0500

    refactor: format TaskController methods for improved readability and add findAllByList method

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -2,11 +2,19 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable prettier/prettier */
 
-
 //*------------------------------------------------------------------
 //* Import Dependency
 //*------------------------------------------------------------------
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+} from '@nestjs/common';
 import { TaskService } from './task.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
@@ -17,8 +25,6 @@ import { FindTaskByUserDto } from './dto/find-task-by-user.dto';
 //*------------------------------------------------------------------
 @Controller('api/task')
 export class TaskController {
-
-
   //*------------------------------------------------------------------
   //* Constructor
   //*----------------------------------------------------------------
@@ -36,11 +42,11 @@ export class TaskController {
   //* Method to find all tasks paginated
   //*----------------------------------------------------------------
   @Get()
-  async findAll(
-    @Param('page') page: number,
-    @Param('limit') limit: number
-  ) {
-    const tasks = await this.taskService.findAll(Number(page) || 1, Number(limit) || 10);
+  async findAll(@Param('page') page: number, @Param('limit') limit: number) {
+    const tasks = await this.taskService.findAll(
+      Number(page) || 1,
+      Number(limit) || 10,
+    );
     return tasks;
   }
 
@@ -48,18 +54,25 @@ export class TaskController {
   //* Method to find all task paginated by user ID
   //*------------------------------------------------------------------
   @Get('user/:id')
-async findAllByUser(
-  @Param('id') id: number,
-  @Query() query: FindTaskByUserDto,
-) {
-  return await this.taskService.findAllByUser(
-    Number(id),
-    query.page || 1,
-    query.limit || 10,
-    query,
-  );
-}
+  async findAllByUser(
+    @Param('id') id: number,
+    @Query() query: FindTaskByUserDto,
+  ) {
+    return await this.taskService.findAllByUser(
+      Number(id),
+      query.page || 1,
+      query.limit || 10,
+      query,
+    );
+  }
 
+  //*------------------------------------------------------------------
+  //* Method to find all task by list ID
+  //*------------------------------------------------------------------
+  @Get('list/:id')
+  async findAllByList(@Param('id') id: number) {
+    return await this.taskService.findAllByList(id);
+  }
   //*------------------------------------------------------------------
   //* Method to find one task
   //*------------------------------------------------------------------

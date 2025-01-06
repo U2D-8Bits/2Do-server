c81a129ab1129190a244a8e7f3df9d9bfe1e4097
Pablo Israel Bolaños Párraga <u2d.8bits@gmail.com>
2025-01-06 11:33:01 -0500

    refactor: improve Task entity structure and formatting for better readability

--- src/task/entities/task.entity.ts
+++ src/task/entities/task.entity.ts
@@ -1,41 +1,45 @@
 /* eslint-disable prettier/prettier */
 
-import { List } from "src/lists/entities/list.entity";
-import { Subtask } from "src/subtask/entities/subtask.entity";
-import { User } from "src/users/entities/user.entity";
-import { Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { List } from 'src/lists/entities/list.entity';
+import { Subtask } from 'src/subtask/entities/subtask.entity';
+import { User } from 'src/users/entities/user.entity';
+import {
+  Column,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class Task {
+  @PrimaryGeneratedColumn()
+  int_task_id: number;
 
-    @PrimaryGeneratedColumn()
-    int_task_id: number;
+  @Column()
+  str_task_title: string;
 
-    @Column()
-    str_task_title: string;
+  @Column({ nullable: true })
+  str_task_description: string;
 
-    @Column({ nullable: true })
-    str_task_description: string;
+  @Column({ nullable: true, type: 'timestamp' })
+  dt_task_due_date: Date;
 
-    @Column({ nullable: true, type: 'timestamp' })
-    dt_task_due_date: Date;
+  @Column({ default: false })
+  bl_task_completed: boolean;
 
-    @Column({ default: false })
-    bl_task_completed: boolean;
+  @Column({ default: 3 })
+  int_task_priority: number;
 
-    @Column({ default: 3})
-    int_task_priority: number;
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  dt_task_created_at: Date;
 
-    @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
-    dt_task_created_at: Date;
+  @ManyToOne(() => User, (user) => user.tasks, { onDelete: 'CASCADE' })
+  user: User;
 
-    @ManyToOne( () => User, (user) => user.tasks, { onDelete: 'CASCADE' })
-    user: User;
+  @ManyToOne(() => List, (list) => list.tasks, { onDelete: 'SET NULL' })
+  list: List;
 
-    @ManyToOne( () => List, (list) => list.tasks, { onDelete: 'CASCADE' })
-    list: List;
-
-    @OneToMany( () => Subtask, (subtask) => subtask.task )
-    subtasks: Subtask[];
-
+  @OneToMany(() => Subtask, (subtask) => subtask.task)
+  subtasks: Subtask[];
 }

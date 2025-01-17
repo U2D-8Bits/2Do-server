fe4b538fc4b2df056b75f87f488e8c0716255d61
Pablo Israel Bolaños Párraga <u2d.8bits@gmail.com>
2025-01-16 22:36:43 -0500

    refactor: make user email, username, and password fields optional in UpdateUserDto and add password confirmation field with validation

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -7,7 +7,7 @@ import { Trim } from 'class-sanitizer';
 export class UpdateUserDto extends PartialType(CreateUserDto) {
   @IsEmail({}, { message: 'El correo electrónico debe ser válido' })
   @Trim()
-  str_user_email: string;
+  str_user_email?: string;
 
   @IsString()
   @MinLength(3, {
@@ -17,7 +17,7 @@ export class UpdateUserDto extends PartialType(CreateUserDto) {
     message: 'El nombre de usuario no puede tener más de 30 caracteres',
   })
   @Trim()
-  str_user_username: string;
+  str_user_username?: string;
 
   @IsString()
   @MinLength(8, { message: 'La contraseña debe tener al menos 8 caracteres' })
@@ -29,5 +29,17 @@ export class UpdateUserDto extends PartialType(CreateUserDto) {
       'La contraseña debe incluir al menos una letra mayúscula, una minúscula, un número y un carácter especial (!@#$%^&*)',
   })
   @Trim()
-  str_user_password: string;
+  str_user_password?: string;
+
+  @IsString()
+  @MinLength(8, { message: 'La contraseña debe tener al menos 8 caracteres' })
+  @MaxLength(20, {
+    message: 'La contraseña no puede tener más de 20 caracteres',
+  })
+  @Matches(/(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*])/, {
+    message:
+      'La contraseña debe incluir al menos una letra mayúscula, una minúscula, un número y un carácter especial (!@#$%^&*)',
+  })
+  @Trim()
+  str_user_password_confirm?: string;
 }

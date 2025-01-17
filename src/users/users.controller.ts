b0a41329eabf6058ce61270182f4414adbbaa4f2
Pablo Israel Bolaños Párraga <u2d.8bits@gmail.com>
2025-01-16 23:05:44 -0500

    feat: implement profile picture upload service for users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,12 +3,17 @@
 /* eslint-disable prettier/prettier */
 
 
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
-import { UsersService } from './users.service';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, UseInterceptors, UploadedFile, HttpException, HttpStatus } from '@nestjs/common';
+
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { LoginDto } from './dto/login.dto';
+
 import { LoginService } from './login.service';
+import { UsersService } from './users.service';
+
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage, Multer } from 'multer';
+import { extname } from 'path';
 
 @Controller('api/users')
 export class UsersController {
@@ -62,6 +67,40 @@ export class UsersController {
     return user;
   }
 
+  //?--------------------------------------------------------------------------------
+  //? Servicio para Actualizar la foto de perfil de un usuario
+  //?--------------------------------------------------------------------------------
+  @Post(':id/upload-profile-picture')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: '../../uploads/profile_pictures',
+        filename: (req, file, cb ) => {
+          const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+          cb(null, `${uniqueSuffix}${extname(file.originalname)}`);
+        }
+      }),
+      fileFilter: (req, file, cb) => {
+        const allowedTypes = ['image/jpeg', 'image/png', 'image/jpg'];
+        if( !allowedTypes.includes(file.mimetype)){
+          cb(new HttpException('Tipo de archivo no permitido', HttpStatus.BAD_REQUEST), false);
+        } else {
+          cb(null, true);
+        }
+      },
+    }),
+  )
+  async uploadProfilePicture(
+    @Param('id') id: number,
+    @UploadedFile() file: Multer.File
+  ){
+    if(!file){
+      throw new HttpException('Archivo no encontrado', HttpStatus.BAD_REQUEST);
+    }
+
+    const filePath = file.filename;
+    return await this.usersService.updateProfilePicture(id, filePath);
+  }
 
   //?--------------------------------------------------------------------------------
   //? Servicio para eliminar un usuario
